dc9b765b2c7b50ae0725795e77120e919ff1ed9a
Saloni Rathi <salonirathi77@gmail.com>
2023-12-30 21:22:48 +0530

    myPortfolio

--- src/About.jsx
+++ src/About.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import photo from './myImg/photo.png';
 
+
 function About() {
+
     return (
         <div className='about' id='About'>
             <div className='aboutbox'>
@@ -10,7 +12,9 @@ function About() {
                     <h1>I'm Fronted Developer <br />Saloni Rathi</h1>
                 </div>
                 <div className='para'>Hii My Name is Saloni Rathi. I am a Fronted Devoloper and I am very passionate and dedicate to my work. I work with HTML, CSS, JavaScript and React. I spend my time on my projects and learning more about HTML, CSS, JavaScript and React. I specialize in JavaScript, React, HTML5, CSS, REST APIs, Git, advanced JavaScript concepts, Redux, and responsive web design.</div>
-                {/* <div className='spanbox'><a href="#"><span>Hire Me</span></a></div> */}
+                <div className='pdfBox'>
+                    <a className='resume' href="resume.pdf" download="esume.pdf">Download Resume</a>
+                </div>
             </div>
             <div className='imgBox'>
                 <img src={photo} alt="" />
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {Link} from "react-scroll"
+import { FaBars, } from "react-icons/fa";
 
 function Navbar() {
+
+    const [showNavbar, setShowNavbar] = useState(false)
+
+    const handleShowNavbar = () => {
+      setShowNavbar(!showNavbar)
+    }
+
     return (
         <div className='navbar'>
             <div className='logo'>
                 <img src="https://websitedemos.net/personal-portfolio-02/wp-content/uploads/sites/770/2021/02/logo-1-70x70.png" alt="img" />
             </div>
 
-            <div className='list'>
-                <ul>
-                    <li><Link to='MainContainer' smooth={true} spy={true}>Home</Link></li>
-                    <li><Link to='About' smooth={true} spy={true}>About</Link></li>
-                    <li><Link to='Projects' smooth={true} spy={true}>Projects</Link></li>
-                    <li><Link to='Skills' smooth={true} spy={true}>Skills</Link></li>
-                    {/* <span><Link to='Contacts' smooth={true} spy={true}>Contact</Link></span> */}
-                </ul>
-                <button><Link to='Contacts' smooth={true} spy={true}>Contact</Link></button>
-            </div>
-            {/* <div className='icon'><i class="fa fa-bars" id='menuIcon'></i></div> */}
+                <div className={`nav-elements  ${showNavbar && 'active'}`}>
+                    <ul>
+                        <li><Link to='MainContainer' smooth={true} spy={true}>Home</Link></li>
+                        <li><Link to='About' smooth={true} spy={true}>About</Link></li>
+                        <li><Link to='Projects' smooth={true} spy={true}>Projects</Link></li>
+                        <li><Link to='Skills' smooth={true} spy={true}>Skills</Link></li>
+                        {/* <span><Link to='Contacts' smooth={true} spy={true}>Contact</Link></span> */}
+                    </ul>
+                </div>
+
+                <div><button className='contactBtn'><Link to='Contacts' smooth={true} spy={true}>Contact</Link></button></div>
+
+                <div className="menu-icon" onClick={handleShowNavbar}>
+                 <FaBars />
+                </div>
 
         </div>
     )
